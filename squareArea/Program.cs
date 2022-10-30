bcc8f93c8200ee188d52aa2d528f2c0a612c7e90
patyb <patyb@DESKTOP-1NDCB4V>
2022-10-30 13:06:56 -0600

    Add project files.

---
+++ squareArea/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace squareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //rectangle area calc.
+            Console.WriteLine("Please enter the sideA of the rectangle, you can use decimals:  ");
+            float sideA = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the sideB of the rectangle , you can use decimals: ");
+            float sideB = float.Parse(Console.ReadLine());
+
+            
+           
+            //rectangle area formula is a*b
+            float area = sideA * sideB;
+
+
+            Console.WriteLine("The rectangle area: " + area);
+
+
+        }
+    }
+}
